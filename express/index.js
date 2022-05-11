83ab54f2c3ddf9beae9ad21ee4a8f9524658b677
minjun <mjk54565@gmail.com>
2022-05-11 14:52:43 +0900

    그래프QL 연습(awit 필요)

--- express/index.js
+++ express/index.js
@@ -1,7 +1,36 @@
 const express = require('express');
-const app = express();
+const { ApolloServer, gql } = require('apollo-server-express');
+
 const port = process.env.PORT || 4000;
+const typeDefs = gql`
+    type Query{
+    hello: String
+    }
+`;
 
-app.get('/', (req, res) => res.send('hello world'));
-app.listen(port, () => console.log('${port}번 포트로 진입'));
+const resolvers = {
+    Query: {
+        hello: () => 'hello world!'
+    }
+}
 
+
+//app.get('/', (req, res) => res.send('hello world'));
+//app.listen({port}, () => console.log(`${port}번 포트로 진입`));
+
+async function startApolloServer(typeDefs, resolvers) {
+    const server = new ApolloServer({ typeDefs, resolvers });
+
+    await server.start();
+
+    const app = express();
+
+    server.applyMiddleware({
+        app, path: '/api'
+    });
+
+    await new Promise(resolve => app.listen({ port: 4000 }, resolve));
+    console.log(`http://localhost:${port}${server.graphqlPath}`);
+}
+
+startApolloServer(typeDefs, resolvers)
